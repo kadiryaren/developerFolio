5a3441ef8797c2aca6940585256704a45363e3fd
kadiryaren <uraniumstar20@gmail.com>
2022-08-01 21:12:00 +0300

    resume updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / PHP and some other cool libraries and frameworks."
   ),
   resumeLink:
-    " https://drive.google.com/file/d/1IWwsnuad2xA3LZnAV4eiBHbHjCFu5sKz/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1lVRqblyihWRIQRNEr3ib0oypJDrPaMSH/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
